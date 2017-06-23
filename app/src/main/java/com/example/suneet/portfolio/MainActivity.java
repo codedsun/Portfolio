ac8b6404401a3db1114cb283b3d9ae403dbb740c
suneet <suneetbond91@gmail.com>
2017-06-23 16:27:29 +0530

    Intent Implemented on Buttons

--- app/src/main/java/com/example/suneet/portfolio/MainActivity.java
+++ app/src/main/java/com/example/suneet/portfolio/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.suneet.portfolio;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,7 +24,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
  TextView v;
  TextView about;
     TextView skillset;
-    TextView ping;
+    TextView ping;ImageButton facebook,twitter,medium,quora;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +40,15 @@ public class MainActivity extends Activity implements View.OnClickListener{
         skillset.setOnClickListener(this);
 
         ping.setOnClickListener(this);
+        facebook=(ImageButton)findViewById(R.id.fb);
+        twitter=(ImageButton)findViewById(R.id.twitter);
+        medium=(ImageButton)findViewById(R.id.medium);
+        quora=(ImageButton)findViewById(R.id.quora);
+        facebook.setOnClickListener(this);
+        twitter.setOnClickListener(this);
+        medium.setOnClickListener(this);
+        quora.setOnClickListener(this);
+
 
     }
 
@@ -44,9 +56,32 @@ public class MainActivity extends Activity implements View.OnClickListener{
     @Override
     public void onClick(View view) {
         v=(TextView)findViewById(R.id.textView);
+
         Typeface typeface=Typeface.createFromAsset(getAssets(),"Roboto.ttf");
         v.setTypeface(typeface);
+        Intent i=new Intent();
+        i.setAction(Intent.ACTION_VIEW);
 
+        if(view.getId()==R.id.fb)
+        {
+            i.setData(Uri.parse("https://www.facebook.com"));
+            startActivity(i);
+        }
+        if(view.getId()==R.id.medium)
+        {
+            i.setData(Uri.parse("https://www.medium.com"));
+            startActivity(i);
+        }
+        if(view.getId()==R.id.twitter)
+        {
+            i.setData(Uri.parse("https://www.twitter.com"));
+            startActivity(i);
+        }
+        if(view.getId()==R.id.quora)
+        {
+            i.setData(Uri.parse("https://www.quora.com"));
+            startActivity(i);
+        }
         if(view.getId()==R.id.aboutme)
         {
                 v.setText(R.string.about_me);
